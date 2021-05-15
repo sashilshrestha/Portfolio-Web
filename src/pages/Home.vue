e7119e88fda0e4af80796eba8402a9eeb98410dd
sashilshrestha <sashilstha5@gmail.com>
2021-05-15 21:49:27 +0545

    Demo Final

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,41 @@
 <template>
     <div id="homePage">
-        <div class="uk-container uk-container-large">
+        <div
+            class="uk-container uk-container-large"
+            uk-scrollspy="cls: uk-animation-slide-left-small; target: .left-side; delay: 300; repeat: true"
+        >
             <div class="left-side">
                 <img src="../assets/Profile.png" alt="" />
             </div>
             <div class="right-side">
                 <div class="my-info">
-                    <h5>Hello !</h5>
-                    <h1>I'm <span>Sashil Shrestha</span></h1>
-                    <p>Front-End Enthusiast, Student, Learner</p>
+                    <h5
+                        uk-scrollspy="cls: uk-animation-slide-left-small; delay: 400; repeat: true"
+                    >
+                        Hello !
+                    </h5>
+                    <h1
+                        uk-scrollspy="cls: uk-animation-slide-left-small; delay: 600; repeat: true"
+                    >
+                        I'm <span>Sashil Shrestha</span>
+                    </h1>
+                    <p
+                        uk-scrollspy="cls: uk-animation-slide-left-small; delay: 800; repeat: true"
+                    >
+                        Front-End Enthusiast, Student, Learner
+                    </p>
                     <div class="cta-sec">
-                        <a href="" class="btn-download">
+                        <a
+                            href=""
+                            class="btn-download"
+                            uk-scrollspy="cls: uk-animation-slide-left-small; delay: 1000; repeat: true"
+                        >
                             Download CV
                         </a>
-                        <div class="social-icons">
+                        <div
+                            class="social-icons"
+                            uk-scrollspy="cls: uk-animation-fade; delay: 300; repeat: true ; target: a"
+                        >
                             <a href="" uk-icon="icon: instagram"></a>
                             <a href="" uk-icon="icon: linkedin"></a>
                             <a href="" uk-icon="icon: github"></a>
@@ -106,12 +128,19 @@ export default {
                     a {
                         &.btn-download {
                             color: #fff;
-                            text-decoration: underline;
+                            // text-decoration: underline;
                             text-transform: uppercase;
                             font-size: 1.125rem;
-                            text-underline-offset: 8px;
+                            // text-underline-offset: 0.5rem;
+                            border-bottom: 1px solid #fff;
                             letter-spacing: 1px;
                             margin: 0 2.5rem 0 0;
+                            transition: all 0.2s ease-in-out;
+
+                            &:hover {
+                                padding-bottom: 0.4rem;
+                                text-decoration: none;
+                            }
                         }
                     }
                     .social-icons {
@@ -127,10 +156,6 @@ export default {
                                 background: #fff;
                                 color: #160929;
                             }
-
-                            svg {
-                                width: 5rem;
-                            }
                         }
                     }
                 }
@@ -145,6 +170,157 @@ export default {
                 top: 0;
                 right: 0;
                 background-size: contain;
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 991px) {
+    #homePage {
+        .uk-container-large {
+            flex-direction: column;
+            .left-side {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                img {
+                    width: 28rem;
+                }
+
+                &::after {
+                }
+            }
+
+            .right-side {
+                .my-info {
+                    padding: 5rem 0 0 0;
+                    h5 {
+                        text-align: center;
+                    }
+                    h1 {
+                        text-align: center;
+                        span {
+                            &::after {
+                            }
+                        }
+                    }
+                    p {
+                        text-align: center;
+                    }
+                    .cta-sec {
+                        flex-direction: column;
+                        a {
+                            margin: 0 0 2rem 0 !important;
+                            &.btn-download {
+                                &:hover {
+                                }
+                            }
+                        }
+                        .social-icons {
+                            a {
+                                margin: 0 0.5rem !important;
+                                &:hover {
+                                }
+                            }
+                        }
+                    }
+                }
+
+                &::after {
+                    display: none;
+                }
+            }
+        }
+    }
+}
+@media screen and (max-width: 400px) {
+    #homePage {
+        .uk-container-large {
+            .left-side {
+                img {
+                }
+
+                &::after {
+                }
+            }
+
+            .right-side {
+                .my-info {
+                    h5 {
+                    }
+                    h1 {
+                        font-size: 4rem;
+                        span {
+                            &::after {
+                            }
+                        }
+                    }
+                    p {
+                    }
+                    .cta-sec {
+                        a {
+                            &.btn-download {
+                                &:hover {
+                                }
+                            }
+                        }
+                        .social-icons {
+                            a {
+                                &:hover {
+                                }
+                            }
+                        }
+                    }
+                }
+
+                &::after {
+                }
+            }
+        }
+    }
+}
+@media screen and (max-width: 361px) {
+    #homePage {
+        .uk-container-large {
+            .left-side {
+                img {
+                }
+
+                &::after {
+                }
+            }
+
+            .right-side {
+                .my-info {
+                    h5 {
+                    }
+                    h1 {
+                        font-size: 3.5rem;
+                        span {
+                            &::after {
+                            }
+                        }
+                    }
+                    p {
+                    }
+                    .cta-sec {
+                        a {
+                            &.btn-download {
+                                &:hover {
+                                }
+                            }
+                        }
+                        .social-icons {
+                            a {
+                                &:hover {
+                                }
+                            }
+                        }
+                    }
+                }
+
+                &::after {
+                }
             }
         }
     }
